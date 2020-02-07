151b4abb0ec6bb3f7507930827ee8212e5851e8e
Jasmine Gallardo <j.m.gallardo6@gmail.com>
2020-02-06 17:37:56 -0800

    deleted semi-colons

--- components/page-header.js
+++ components/page-header.js
@@ -1,7 +1,7 @@
 class PageHeader {
   constructor(headerElement) {
-    this.headerElement = headerElement;
-    this.updateAverage = this.updateAverage.bind(this);
+    this.headerElement = headerElement
+    this.updateAverage = this.updateAverage.bind(this)
   }
   updateAverage(newAverage) {
     var badgeElement = this.headerElement.querySelector(".badge");
