7168397fe72f5a403c049183525eab23a5fa99ab
Jasmine Gallardo <j.m.gallardo6@gmail.com>
2020-02-08 15:30:08 -0800

    Modified updateGrades() method to use this.renderGradeRow() to append delete grade rows to DOM.

--- components/grade-table.js
+++ components/grade-table.js
@@ -6,20 +6,7 @@ class GradeTable {
     var tbodyElement = this.tableElement.querySelector("tbody");
     tbodyElement.textContent = "";
     for (var i = 0; i < grades.length; i++){
-      var trElement = document.createElement("tr");
-      var name = document.createElement("td");
-      name.textContent = grades[i].name;
-
-      var course = document.createElement("td");
-      course.textContent = grades[i].course;
-
-      var grade = document.createElement("td");
-      grade.textContent = grades[i].grade;
-
-      trElement.appendChild(name);
-      trElement.appendChild(course);
-      trElement.appendChild(grade);
-      tbodyElement.appendChild(trElement);
+      this.renderGradeRow(grades[i], this.deleteGrade);
     }
     console.log("Grades:", grades);
   }
@@ -27,22 +14,21 @@ class GradeTable {
     this.deleteGrade = deleteGrade;
   }
   renderGradeRow(data, deleteGrade) {
-    console.log(data);
     var tbodyElement2 = this.tableElement.querySelector("tbody");
 
     var trElementRender = document.createElement("tr");
 
-    var tdNameRender = createElement("td");
+    var tdNameRender = document.createElement("td");
     tdNameRender.textContent = data.name;
 
-    var tdCourseRender = createElement("td");
+    var tdCourseRender = document.createElement("td");
     tdCourseRender.textContent = data.course;
 
-    var tdGradeRender = createElement("td");
+    var tdGradeRender = document.createElement("td");
     tdGradeRender.textContent = data.grade;
 
-    var tdRenderForButton = createElement("td");
-    var deleteButtonRender = createElement("button");
+    var tdRenderForButton = document.createElement("td");
+    var deleteButtonRender = document.createElement("button");
     deleteButtonRender.textContent = "DELETE";
     deleteButtonRender.className = "btn btn-danger";
     deleteButtonRender.setAttribute("type", "button");
