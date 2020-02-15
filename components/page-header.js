df81cab70d881754b0ceee71c9b1712d56a25365
Jasmine Gallardo <j.m.gallardo6@gmail.com>
2020-02-15 13:52:45 -0800

    Added conditional to display N/A in average grade header when there are no grades displayed.

--- components/page-header.js
+++ components/page-header.js
@@ -3,8 +3,12 @@ class PageHeader {
     this.headerElement = headerElement
     this.updateAverage = this.updateAverage.bind(this)
   }
-  updateAverage(newAverage) {
+  updateAverage(newAverage, grades) {
     var badgeElement = this.headerElement.querySelector(".badge");
     badgeElement.textContent = newAverage;
+    if (grades.length === 0){
+      badgeElement.classList.add("badge-secondary");
+      badgeElement.textContent = "N/A";
+    }
   }
 }
